f71bafcddaf3b332f01bcf50a14969c12d5a41e2
KarinaSelingardi <133906857+KarinaSelingardi@users.noreply.github.com>
2023-06-15 10:27:35 -0300

    teste strings

---
+++ strings.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    char s1[80],s2[80];
+    
+    gets(s1);
+    gets(s2);
+    
+    printf("comprimentos: %d %d\n", strlen(s1),strlen(s2));
+    
+    if(!strcmp(s1,s2))printf("as strings são iguais\n");
+    strcat(s1, s2);
+    printf("%s\n",s1);
+    
+    strcpy(s1,"isso é um teste.\n");
+    printf(s1);
+    if(strchr("alo",'o')) printf(" o está em alo\n");
+    if( strstr("ola aqui","ola"))printf("ola encontrado");
+}
+
+
